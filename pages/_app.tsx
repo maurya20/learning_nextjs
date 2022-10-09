8e6bf322d14722b00c24b2f0bd2ec8874761e239
ind20 <indresh.maurya@gmail.com>
2022-10-09 11:44:50 +0530

    basic routing added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="header">
+        <Link href="/">
+          <a className="link">Home</a>
+        </Link>
+        <Link href="/about">
+          <a className="link">About</a>
+        </Link>
+      </div>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
